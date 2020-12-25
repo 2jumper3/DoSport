4eabb90fd4074ba5bcbf11f343f968f96cccabd8
Kamol <rusmirolub@gmail.com>
2020-12-25 08:14:33 +0000

    [iOS-13] commiting missed files

--- DoSport/DoSport/Helpers/Models/UserRegion.swift
+++ DoSport/DoSport/Helpers/Models/UserRegion.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct UserRegion {
+    let region: Region
+    let code: RegionCode
+}
+
+enum RegionCode: Int, CaseIterable {
+    case c_994 = 994
+    
+}
+
+enum Region: String, CaseIterable {
+    case australia
+    case austria
+    case azerbaijan
+}
