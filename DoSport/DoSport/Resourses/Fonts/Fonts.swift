f14c066c86bfb0d91c8dbf60f4dd6950a5610394
Kamol <rusmirolub@gmail.com>
2021-02-25 20:44:58 +0000

    [iOS-35] Fixed remained UI bugs.

--- DoSport/DoSport/Resourses/Fonts/Fonts.swift
+++ DoSport/DoSport/Resourses/Fonts/Fonts.swift
@@ -5,13 +5,11 @@
 //  Created by Komolbek Ibragimov on 18/12/2020.
 //
 
-import Foundation
 import UIKit
 
 public enum Fonts {
     static func sfProRegular( size: CGFloat) -> UIFont {
         var localSize = size
-        print(UIDevice.deviceSize)
         switch UIDevice.deviceSize {
         case .iPhone_5_5S_5C_SE1: localSize -= 1
         case .iPhone_6_6S_7_8_SE2: break
