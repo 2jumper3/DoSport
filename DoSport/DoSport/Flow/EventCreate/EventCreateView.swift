f41c67901c2de9b4f33dcc95a47703151026080d
Kamol <rusmirolub@gmail.com>
2021-02-26 01:44:46 +0000

    [iOS-42] Fixed cells heigh. Fixed save button's layout. Made table style grouped. Added footer view

--- DoSport/DoSport/Flow/EventCreate/EventCreateView.swift
+++ DoSport/DoSport/Flow/EventCreate/EventCreateView.swift
@@ -18,11 +18,12 @@ final class EventCreateView: UIView {
     // MARK: Outlets
     
     private lazy var tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .plain)
+        let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.registerClass(TextViewCell.self)
         tableView.registerClass(EventTypeCell.self)
         tableView.registerClass(SelectionCell.self)
         tableView.registerClass(MembersCountCell.self)
+        tableView.registerHeaderFooter(EventTypeTableCellFooter.self)
         tableView.keyboardDismissMode = .onDrag
         tableView.separatorColor = Colors.dirtyBlue
         tableView.backgroundColor = Colors.darkBlue
@@ -51,17 +52,32 @@ final class EventCreateView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         
+        var buttonsHeight: CGFloat = 48
+        var buttonBottom: CGFloat = 15
+        
+        switch UIDevice.deviceSize {  // FIXME: костыль?
+        case .iPhone_5_5S_5C_SE1, .iPhone_6_6S_7_8_SE2:
+            buttonsHeight = 40
+        case .iPhone_6_6S_7_8_PLUS, .iPhone_X_XS_12mini:
+            buttonsHeight = 46
+            buttonBottom += 15
+        case .iPhone_XR_11, .iPhone_XS_11Pro_Max, .iPhone_12_Pro, .iPhone_12_Pro_Max:
+            buttonsHeight = 48
+            buttonBottom += 20
+        default: break
+        }
+        
         tableView.snp.makeConstraints {
             $0.centerX.width.equalToSuperview()
             $0.top.equalTo(safeAreaInsets.top).offset(2)
-            $0.bottom.equalTo(createButton.snp.top).offset(-25)
+            $0.bottom.equalTo(createButton.snp.top).offset(createButton.frame.height/2)
         }
         
         createButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.width.equalToSuperview().multipliedBy(0.89)
-            $0.height.equalTo(42)
-            $0.bottom.equalTo(safeAreaInsets.bottom).offset(-10)
+            $0.height.equalTo(buttonsHeight)
+            $0.bottom.equalTo(safeAreaInsets.bottom).offset(-buttonBottom)
         }
     }
 }
