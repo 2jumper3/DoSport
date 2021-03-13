1a8b9649cd47d73271dd329328f948fbac942ae8
Kamol <rusmirolub@gmail.com>
2021-03-13 16:37:11 +0000

    [iOS-54] Refactored AppDelegate class by removing UserNotification and Firebase api.

--- DoSport/DoSport/Application/AppDelegate.swift
+++ DoSport/DoSport/Application/AppDelegate.swift
@@ -19,15 +19,11 @@ class AppDelegate : UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions
         launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
         
-        FirebaseApp.configure()
-        
         if #available(iOS 13, *) {
-            UNUserNotificationCenter.current().delegate = self
+            let pushManager = DSPushNotificationManager(userID: "currently_logged_in_user_id") // TODO: finish UN setup
+            pushManager.registerForPushNotifications()
             
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { (_, _) in }
-            Messaging.messaging().delegate = self
-            
+            FirebaseApp.configure()
             /// call this when app gets permission from user to send notifications to enable FCM to start configuration.
 //            Messaging.messaging().autoInitEnabled = true
             
@@ -50,14 +46,6 @@ class AppDelegate : UIResponder, UIApplicationDelegate {
         return true
     }
     
-    /// Tells the delegate that the app successfully registered with Apple Push Notification service
-    func application(
-        _ application: UIApplication,
-        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
-    ) {
-        Messaging.messaging().apnsToken = deviceToken
-    }
-    
     /// Handle when app enters background when user leaves app
     func applicationDidEnterBackground(_ application: UIApplication) {
         print(#function)
@@ -69,57 +57,6 @@ class AppDelegate : UIResponder, UIApplicationDelegate {
     
     /// Called when Firebase registration token is received
     @objc func handleReceiveMessagingFromFirebase() {
-        
-    }
-}
-
-//MARK: - UNUserNotificationCenterDelegate -
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-    ) {
-        let userInfo = notification.request.content.userInfo
-        print(userInfo)
-        
-        Messaging.messaging().appDidReceiveMessage(userInfo)
-        
-        completionHandler([[.alert, .sound]])
-    }
-    
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        didReceive response: UNNotificationResponse,
-        withCompletionHandler completionHandler: @escaping () -> Void
-    ) {
-        let userInfo = response.notification.request.content.userInfo
-        print(userInfo)
-        
-        Messaging.messaging().appDidReceiveMessage(userInfo)
-        
-        completionHandler()
-    }
-    
-    func application(
-        _ application: UIApplication,
-        didReceiveRemoteNotification userInfo: [AnyHashable : Any],
-        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
-    ) {
-        print(userInfo)
-        Messaging.messaging().appDidReceiveMessage(userInfo)
-        completionHandler(.noData)
-    }
-}
-
-//MARK: - Firebase | MessagingDelegate -
-
-extension AppDelegate: MessagingDelegate {
-
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("Firebase registration token: \(String(describing: fcmToken))")
         
     }
 }
