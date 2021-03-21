32a3a4e0acd26705c884d25211fde5d6ba65be40
Kamol <rusmirolub@gmail.com>
2021-03-21 11:51:15 +0000

    [#76] Implemented all endpoint components for User requests

--- DoSport/DoSport/Helpers/DSApi/endpoints/api/DSEndpoints+User.swift
+++ DoSport/DoSport/Helpers/DSApi/endpoints/api/DSEndpoints+User.swift
@@ -23,6 +23,8 @@ extension DSEndpoints {
         case addSubscriptionByID(DSUserProfileRequests.UserProfileById)
         case deleteSubscriptionByID(DSUserProfileRequests.UserProfileById)
         
+        //MARK: - Path -
+        
         var path: String {
             switch self {
             case .getProfileByID(let userProfile):             return "profiles/\(userProfile.id)"
@@ -35,6 +37,41 @@ extension DSEndpoints {
             case .addSubscriptionByID(let user):               return "profiles/subscriptions/\(user.id)"
             case .deleteSubscriptionByID(let user):            return "profiles/subscriptions/\(user.id)"
             default:                                           return "profiles"
+            }
+        }
+        
+        //MARK: - ParameterObject -
+
+        var parameters: ParameterObject? {
+            switch self {
+            case .editProfile(let user): return user
+            case .editPreferredSportTypes(let sportTypes): return sportTypes
+            default: return nil
+            }
+        }
+        
+        //MARK: - ParameterEncoding -
+
+        var parameterEncoding: ParameterEncoding {
+            switch self {
+            case .editProfile:             return .jsonEncoding
+            case .editPreferredSportTypes: return .jsonEncoding
+            default:                       return .urlEncoding
+            }
+        }
+        
+        //MARK: - HTTPMethod -
+
+        var method: HTTPMethod {
+            switch self {
+            case .editProfile:                  return .put
+            case .deleteProfile:                return .delete
+            case .editPreferredSportTypes:      return .put
+            case .addPreferredSportTypeByID:    return .put
+            case .deletePreferredSportTypeByID: return .delete
+            case .addSubscriptionByID:          return .put
+            case .deleteSubscriptionByID:       return .delete
+            default:                            return .get
             }
         }
     }
