cffa2b720ff73a659c0a6aea8f185eeb59573522
Maxim Safronov <safronov87@gmail.com>
2021-03-25 12:23:06 +0300

    [#79] DeepLinkService with destination modules added

---
+++ DoSport/DoSport/Helpers/DeepLinks/DeepLinkService.swift
@@ -0,0 +1,47 @@
+//
+//  DeepLinkService.swift
+//  DoSport
+//
+//  Created by Maxim Safronov on 20.03.2021.
+//
+
+import Foundation
+
+enum DeepLink {
+    enum Url {
+        static let event = "dosport://feed/event/"
+    }
+}
+ 
+final class DeepLinkComponents {
+    var url: URL
+    var coordinator: AppCoordinator?
+    
+    init(url: URL, coordinator: AppCoordinator) {
+        self.url = url
+        self.coordinator = coordinator
+    }
+}
+
+protocol DeepLinkDestinationModule {
+    func getDestination(components: DeepLinkComponents)
+}
+
+protocol DeepLinkServiceProtocol: class {
+    func handleURL(with components: DeepLinkComponents)
+}
+
+class DeepLinkService: DeepLinkServiceProtocol {
+    
+    var destinations: [DeepLinkDestinationModule]
+    
+    init(destinations: [DeepLinkDestinationModule]) {
+        self.destinations = destinations
+    }
+    
+    func handleURL(with components: DeepLinkComponents) {
+        destinations.forEach { destination in
+            destination.getDestination(components: components)
+        }
+    }
+}
